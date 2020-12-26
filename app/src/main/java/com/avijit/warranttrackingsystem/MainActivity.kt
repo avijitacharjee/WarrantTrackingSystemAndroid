5d59f3418569a1930c72a540422b893af4bda64b
AvijitAcharjee <avijitach@gmail.com>
2020-12-26 15:42:01 +0600

    splash screen

--- app/src/main/java/com/avijit/warranttrackingsystem/MainActivity.kt
+++ app/src/main/java/com/avijit/warranttrackingsystem/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.avijit.warranttrackingsystem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.avijit.warranttrackingsystem.databinding.ActivityMainBinding
+import com.avijit.warranttrackingsystem.ui.activity.AuthActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -10,5 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this,AuthActivity::class.java))
+        },2000)
     }
 }
---
+++ app/src/main/java/com/avijit/warranttrackingsystem/ui/activity/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.avijit.warranttrackingsystem.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.avijit.warranttrackingsystem.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
