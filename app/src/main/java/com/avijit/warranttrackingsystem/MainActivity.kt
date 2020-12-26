f9cca123a3570a8366c9adac2cac311d0002a832
AvijitAcharjee <avijitach@gmail.com>
2020-12-26 15:13:43 +0600

    View Binding

--- app/src/main/java/com/avijit/warranttrackingsystem/MainActivity.kt
+++ app/src/main/java/com/avijit/warranttrackingsystem/MainActivity.kt
@@ -2,11 +2,14 @@ package com.avijit.warranttrackingsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.avijit.warranttrackingsystem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
     }
 }
--- app/src/main/java/com/avijit/warranttrackingsystem/utils/Constants.kt
+++ app/src/main/java/com/avijit/warranttrackingsystem/utils/Constants.kt
@@ -4,4 +4,4 @@ package com.avijit.warranttrackingsystem.utils
  * Created by Avijit Acharjee on 12/26/2020 at 1:13 PM.
  * Email: avijitach@gmail.com.
  */
-
+const val APP_NAME = "Warrant Tracking System"
