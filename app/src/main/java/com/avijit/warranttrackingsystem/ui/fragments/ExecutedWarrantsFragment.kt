2cc8badab050e9ddd6c9ed9a833f9de996b09c16
AvijitAcharjee <avijitach@gmail.com>
2021-01-04 10:42:18 +0600

    executionFragment

---
+++ app/src/main/java/com/avijit/warranttrackingsystem/ui/fragments/ExecutedWarrantsFragment.kt
@@ -0,0 +1,11 @@
+package com.avijit.warranttrackingsystem.ui.fragments
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Avijit Acharjee on 1/4/2021 at 10:39 AM.
+ * Email: avijitach@gmail.com.
+ */
+class ExecutedWarrantsFragment : Fragment() {
+
+}
