e09d50afb3e852898f3b583a8c7c9ee224e3fb8d
AvijitAcharjee <avijitach@gmail.com>
2021-01-06 11:25:17 +0600

    nav

--- app/src/main/java/com/avijit/warranttrackingsystem/ui/fragments/ExecutedWarrantsFragment.kt
+++ app/src/main/java/com/avijit/warranttrackingsystem/ui/fragments/ExecutedWarrantsFragment.kt
@@ -1,11 +1,24 @@
 package com.avijit.warranttrackingsystem.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.avijit.warranttrackingsystem.databinding.FragmentExecutedWarrantsBinding
 
 /**
  * Created by Avijit Acharjee on 1/4/2021 at 10:39 AM.
  * Email: avijitach@gmail.com.
  */
 class ExecutedWarrantsFragment : Fragment() {
-
+    private lateinit var binding : FragmentExecutedWarrantsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentExecutedWarrantsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
 }
